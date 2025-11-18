28d94f2b461e290aa357345d1f6047b50478b8bb
MarcusGrass <marcus.grass@protonmail.com>
2025-11-18 22:37:24 +0100

    fix: refmt boot main

--- boot-rs/src/main.rs
+++ boot-rs/src/main.rs
@@ -5,7 +5,6 @@
 
 extern crate alloc;
 
-use uefi::Status;
 use alloc::string::{String, ToString};
 use alloc::vec::Vec;
 use alloc::{format, vec};
@@ -13,14 +12,15 @@ use boot_lib::crypt::BootDecryptError;
 use boot_lib::BootCfg;
 use core::fmt::Write;
 use uefi::boot::{LoadImageSource, SearchType};
-use uefi::runtime::Time;
 use uefi::proto::console::text::Key;
 use uefi::proto::device_path::text::{AllowShortcuts, DevicePathToText, DisplayOnly};
 use uefi::proto::device_path::DevicePath;
 use uefi::proto::media::file::{File, FileAttribute, FileInfo, FileMode};
 use uefi::proto::media::fs::SimpleFileSystem;
 use uefi::proto::ProtocolPointer;
+use uefi::runtime::Time;
 use uefi::CStr16;
+use uefi::Status;
 
 const CFG_RAW: &str = include_str!("../../boot.cfg");
 
@@ -39,8 +39,9 @@ fn main() -> Status {
         // Borrow checker, the above borrow doesn't allow us to use stdout.
         Err(e) => e,
     };
-    let _ = uefi::system::with_stdout(|output|
-        output.write_fmt(format_args!("[boot-rs]: Failed to boot: {e}.\n")));
+    let _ = uefi::system::with_stdout(|output| {
+        output.write_fmt(format_args!("[boot-rs]: Failed to boot: {e}.\n"))
+    });
     await_enter();
     Status::LOAD_ERROR
 }
@@ -53,33 +54,25 @@ fn boot() -> Result<(), String> {
     let encrypted_kernel = read_kernel_data::<SimpleFileSystem>(&boot_cfg)?;
     let _ = uefi::system::with_stdout(|output| output.write_str("[boot-rs]: Read kernel file.\n"));
     let decrypted_kernel = decrypt_kernel(&encrypted_kernel)?;
-    let _ = uefi::system::with_stdout(|output| output.write_str("[boot-rs]: Decrypted kernel file, loading kernel image.\n"));
+    let _ = uefi::system::with_stdout(|output| {
+        output.write_str("[boot-rs]: Decrypted kernel file, loading kernel image.\n")
+    });
     yield_to_kernel(decrypted_kernel)
 }
 
-fn read_kernel_data<P: ProtocolPointer + ?Sized>(
-    cfg: &BootCfg,
-) -> Result<Vec<u8>, String> {
-    let hb = uefi::boot::locate_handle_buffer(SearchType::ByProtocol(&P::GUID))
-        .map_err(|e| {
-            format!("ERROR: Failed to locate handle buffers for SimpleFileSystem: {e:?}")
-        })?;
-    let to_text_handle =
-        uefi::boot::get_handle_for_protocol::<DevicePathToText>()
-        .unwrap();
-    let to_text_path_protoc = uefi::boot::open_protocol_exclusive::<DevicePathToText>(to_text_handle)
-        .unwrap();
+fn read_kernel_data<P: ProtocolPointer + ?Sized>(cfg: &BootCfg) -> Result<Vec<u8>, String> {
+    let hb = uefi::boot::locate_handle_buffer(SearchType::ByProtocol(&P::GUID)).map_err(|e| {
+        format!("ERROR: Failed to locate handle buffers for SimpleFileSystem: {e:?}")
+    })?;
+    let to_text_handle = uefi::boot::get_handle_for_protocol::<DevicePathToText>().unwrap();
+    let to_text_path_protoc =
+        uefi::boot::open_protocol_exclusive::<DevicePathToText>(to_text_handle).unwrap();
     for handle in hb.iter() {
-        if let Ok(dev_protoc) = uefi::boot::open_protocol_exclusive::<DevicePath>(*handle)
-        {
+        if let Ok(dev_protoc) = uefi::boot::open_protocol_exclusive::<DevicePath>(*handle) {
             let mut found_tgt = false;
             for node in dev_protoc.node_iter() {
                 let text = to_text_path_protoc
-                    .convert_device_node_to_text(
-                        node,
-                        DisplayOnly(false),
-                        AllowShortcuts(true),
-                    )
+                    .convert_device_node_to_text(node, DisplayOnly(false), AllowShortcuts(true))
                     .unwrap();
                 if text.to_string().to_lowercase() == cfg.device.to_lowercase() {
                     found_tgt = true;
@@ -93,7 +86,8 @@ fn read_kernel_data<P: ProtocolPointer + ?Sized>(
                 // I don't know why there aren't conversion methods for this, maybe I'm doing something weird
                 // Safety: No pointer conversion, no messing with the pointer
                 let mut dev_ptr = unsafe { DevicePath::from_ffi_ptr(node_ptr) };
-                if let Ok(can_load_fs) = uefi::boot::locate_device_path::<SimpleFileSystem>(&mut dev_ptr)
+                if let Ok(can_load_fs) =
+                    uefi::boot::locate_device_path::<SimpleFileSystem>(&mut dev_ptr)
                 {
                     let mut loaded = uefi::boot::open_protocol_exclusive::<SimpleFileSystem>(can_load_fs)
                         .map_err(|e| format!("ERROR: Failed to load SimpleFileSystem protocol on an handle checked to be able to load it: {e:?}"))?;
@@ -128,7 +122,11 @@ fn read_kernel_data<P: ProtocolPointer + ?Sized>(
 fn decrypt_kernel(buf: &[u8]) -> Result<Vec<u8>, String> {
     for i in 1..4 {
         let key = get_pass()?;
-        let _ = uefi::system::with_stdout(|output| output.write_fmt(format_args!("[boot-rs]: Got password, deriving key and attempting decrypt.\n")));
+        let _ = uefi::system::with_stdout(|output| {
+            output.write_fmt(format_args!(
+                "[boot-rs]: Got password, deriving key and attempting decrypt.\n"
+            ))
+        });
 
         let t0 = uefi::runtime::get_time();
         match boot_lib::crypt::hash_and_decrypt(buf, key.as_bytes()) {
@@ -138,9 +136,11 @@ fn decrypt_kernel(buf: &[u8]) -> Result<Vec<u8>, String> {
                     #[allow(clippy::cast_precision_loss)]
                     if let Some(delta) = get_time_delta_millis(t0, t1) {
                         let seconds = delta as f32 / 1000f32;
-                        let _ = uefi::system::with_stdout(|output| output.write_fmt(format_args!(
+                        let _ = uefi::system::with_stdout(|output| {
+                            output.write_fmt(format_args!(
                             "[boot-rs]: Derived key and decrypted kernel in {seconds} seconds.\n"
-                        )));
+                        ))
+                        });
                         false
                     } else {
                         true
@@ -149,17 +149,21 @@ fn decrypt_kernel(buf: &[u8]) -> Result<Vec<u8>, String> {
                     true
                 };
                 if failed_time {
-                    let _ = uefi::system::with_stdout(|output| output.write_fmt(format_args!(
-                        "[boot-rs]: Derived key and decrypted kernel.\n"
-                    )));
+                    let _ = uefi::system::with_stdout(|output| {
+                        output.write_fmt(format_args!(
+                            "[boot-rs]: Derived key and decrypted kernel.\n"
+                        ))
+                    });
                 }
                 return Ok(tgt);
             }
             Err(e) => match e {
                 BootDecryptError::InvalidContent => {
-                    let _ = uefi::system::with_stdout(|output| output.write_fmt(format_args!(
-                        "[boot-rs]: Failed to decrypt kernel, bad pass, attempt [{i}/3].\n"
-                    )));
+                    let _ = uefi::system::with_stdout(|output| {
+                        output.write_fmt(format_args!(
+                            "[boot-rs]: Failed to decrypt kernel, bad pass, attempt [{i}/3].\n"
+                        ))
+                    });
                 }
                 BootDecryptError::Other(o) => {
                     return Err(format!("ERROR: Failed to decrypt kernel image: {o}"));
@@ -170,38 +174,37 @@ fn decrypt_kernel(buf: &[u8]) -> Result<Vec<u8>, String> {
     Err("ERROR: Failed to decrypt kernel image, too many failed attempts".to_string())
 }
 
-fn yield_to_kernel(
-    mut raw_kernel_image: Vec<u8>,
-) -> Result<(), String> {
+fn yield_to_kernel(mut raw_kernel_image: Vec<u8>) -> Result<(), String> {
     let self_h = uefi::boot::image_handle();
     // Data is copied, it's okay to drop the buffer after this code executes
     let loaded_kernel = uefi::boot::load_image(
-            self_h,
-            LoadImageSource::FromBuffer {
-                buffer: &mut raw_kernel_image,
-                file_path: None,
-            },
-        )
-        .map_err(|e| format!("ERROR: Failed to load kernel image: {e:?}"))?;
+        self_h,
+        LoadImageSource::FromBuffer {
+            buffer: &mut raw_kernel_image,
+            file_path: None,
+        },
+    )
+    .map_err(|e| format!("ERROR: Failed to load kernel image: {e:?}"))?;
     uefi::boot::start_image(loaded_kernel)
         .map_err(|e| format!("ERROR: Failed to yield execution to the kernel image: {e:?}"))
 }
 
 fn get_pass() -> Result<String, String> {
-    let _ = uefi::system::with_stdout(|output| output.write_str("[boot-rs]: Enter kernel decryption key: \n"));
+    let _ = uefi::system::with_stdout(|output| {
+        output.write_str("[boot-rs]: Enter kernel decryption key: \n")
+    });
     let mut decr = String::new();
     loop {
         // Safety:
         // Safe if event is not reused after close (we only oneshot it).
 
-        let Some(key_ready_evt) = uefi::system::with_stdin(|input| input.wait_for_key_event()) else {
+        let Some(key_ready_evt) = uefi::system::with_stdin(|input| input.wait_for_key_event())
+        else {
             continue;
         };
         uefi::boot::wait_for_event(&mut [key_ready_evt])
             .map_err(|e| format!("ERROR: Failed to wait for key event when listening to kernel decryption key: {e:?}"))?;
-        let key = uefi::system::with_stdin(|input| {
-            input.read_key()
-        })
+        let key = uefi::system::with_stdin(|input| input.read_key())
             .map_err(|e| {
                 format!("ERROR: Failed to read key after waiting and receiving a key event: {e:?}")
             })?
@@ -216,6 +219,9 @@ fn get_pass() -> Result<String, String> {
                 if ch == '\r' {
                     break;
                 }
+                if ch == '\u{08}' {
+                    decr.pop();
+                }
                 decr.push(ch);
             }
             Key::Special(_) => {}
@@ -225,18 +231,17 @@ fn get_pass() -> Result<String, String> {
 }
 
 fn await_enter() {
-    let _ = uefi::system::with_stdout(|output| output.write_str("[boot-rs]: Press enter to exit.\n"));
-    let _ = uefi::system::with_stdin(|input| {
-        loop {
-            if let Some(key) = input.read_key().unwrap() {
-                match key {
-                    Key::Printable(ch) => {
-                        if char::from(ch) == '\r' {
-                            break;
-                        }
+    let _ =
+        uefi::system::with_stdout(|output| output.write_str("[boot-rs]: Press enter to exit.\n"));
+    let _ = uefi::system::with_stdin(|input| loop {
+        if let Some(key) = input.read_key().unwrap() {
+            match key {
+                Key::Printable(ch) => {
+                    if char::from(ch) == '\r' {
+                        break;
                     }
-                    Key::Special(_) => {}
                 }
+                Key::Special(_) => {}
             }
         }
     });
